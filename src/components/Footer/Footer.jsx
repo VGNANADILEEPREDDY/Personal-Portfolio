d19db0040562c9d776ebaf88d475f5938cdc4373
Dileep_742 <167205876+VGNANADILEEPREDDY@users.noreply.github.com>
2025-11-03 08:46:42 +0530

    new

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,11 +38,11 @@ const Footer = () => {
         {/* Social Media Icons - Responsive */}
         <div className="flex flex-wrap justify-center space-x-4 mt-6">
           {[
-            { icon: <FaFacebook />, link: "" },
-            { icon: <FaTwitter />, link: "#" },
+            // { icon: <FaFacebook />, link: "" },
+            // { icon: <FaTwitter />, link: "#" },
             { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/gnana-dileep-reddy-v-a67790254?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2F6vSyYEQSX6YidNg6zXiPw%3D%3D" },
-            { icon: <FaInstagram />, link: "#" },
-            { icon: <FaYoutube />, link: "#" },
+            // { icon: <FaInstagram />, link: "#" },
+            // { icon: <FaYoutube />, link: "#" },
             
           ].map((item, index) => (
             <a
